9984b1e2bb16f838d2e648ac45fa42d838d5580c
Supasan Suklim <supasan.game@gmail.com>
2020-01-30 14:47:02 +0700

    Remove log file

--- test.sh
+++ test.sh
@@ -16,17 +16,20 @@ R_DIR=${T_DIR}/report
 rm -rf ${R_DIR} > /dev/null 2>&1
 mkdir -p ${R_DIR}
 
-/bin/rm -f ${T_DIR}/test-plan.jtl ${T_DIR}/jmeter.log  > /dev/null 2>&1
+/bin/rm -f ${T_DIR}/${JTL_FILE} ${T_DIR}/${LOG_FILE}  > /dev/null 2>&1
 
 ./run.sh -Dlog_level.jmeter=DEBUG \
 	-n -t ${T_DIR}/${JMX_FILE} -l ${T_DIR}/${JTL_FILE} -j ${T_DIR}/${LOG_FILE} \
 	-e -o ${R_DIR}
 
 echo "==== jmeter.log ===="
-cat ${T_DIR}/jmeter.log
+cat ${T_DIR}/${LOG_FILE}
 
 echo "==== Raw Test Report ===="
-cat ${T_DIR}/test-plan.jtl
+cat ${T_DIR}/${JTL_FILE}
 
 echo "==== HTML Test Report ===="
 echo "See HTML test report in ${R_DIR}/index.html"
+
+echo "==== JTL Test Report ===="
+echo "pbcopy < ${R_DIR}/index.html"
