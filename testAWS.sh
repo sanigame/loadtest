ae53da13a5e4be3b150e76b20aa2d6fb231936a0
Pornchai Lerthathairat <>
2020-02-06 11:41:19 +0700

    Update AWS & GCP test script

---
+++ testAWS.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#
+# Test the JMeter Docker image using a trivial test plan.
+
+# Example for using User Defined Variables with JMeter
+# These will be substituted in JMX test script
+# See also: http://stackoverflow.com/questions/14317715/jmeter-changing-user-defined-variables-from-command-line
+
+T_DIR=tests/krungthai-next
+JMX_FILE=Transfer_Intranal_KTB_to_KTB_AWS.jmx
+JTL_FILE=Transfer_Intranal_KTB_to_KTB_AWS.jtl
+LOG_FILE=jmeter.log
+
+# Reporting dir: start fresh
+R_DIR=${T_DIR}/report
+rm -rf ${R_DIR} > /dev/null 2>&1
+mkdir -p ${R_DIR}
+
+/bin/rm -f ${T_DIR}/${JTL_FILE} ${T_DIR}/${LOG_FILE}  > /dev/null 2>&1
+
+./run.sh -Dlog_level.jmeter=DEBUG \
+    -Jthreads=500 -Jrampup=600 \
+	-n -t ${T_DIR}/${JMX_FILE} -l ${T_DIR}/${JTL_FILE} -j ${T_DIR}/${LOG_FILE} \
+	-e -o ${R_DIR}
+
+echo "==== jmeter.log ===="
+cat ${T_DIR}/${LOG_FILE}
+
+# echo "==== Raw Test Report ===="
+# cat ${T_DIR}/${JTL_FILE}
+
+echo "==== HTML Test Report ===="
+echo "See HTML test report in ${R_DIR}/index.html"
+
+echo "==== JTL Test Report ===="
+echo "pbcopy < tests/${JTL_FILE}"
